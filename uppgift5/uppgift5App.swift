2bf76a96e1be3c8309a46f260c8e86e852fb0fa2
antonsmedberg <anton@smedberg.eu>
2023-10-24 12:45:58 +0200

    Initial Commit

---
+++ uppgift5/uppgift5App.swift
@@ -0,0 +1,17 @@
+//
+//  uppgift5App.swift
+//  uppgift5
+//
+//  Created by Anton Smedberg on 2023-10-24.
+//
+
+import SwiftUI
+
+@main
+struct uppgift5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
